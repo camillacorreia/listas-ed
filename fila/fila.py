cb96c47945a911385dbe7bfb55b0ac5fb613932c
Camilla Correia <camillajesuscorreia@hotmail.com>
2023-04-23 12:02:45 -0300

    métodos enfileirar e desenfileirar

---
+++ fila/fila.py
@@ -0,0 +1,34 @@
+class Fila:
+  def __init__(self, maxelem):
+    self.fila = [0] * maxelem
+    self.tamanho = 0
+
+  def enfileirar(self, x):
+    i = 0
+    while i < self.tamanho and self.fila[i] != x:
+      i += 1
+    if i == self.tamanho:
+      if self.tamanho < len(self.fila):
+        self.fila[self.tamanho] = x
+        self.tamanho += 1
+
+  def desenfileirar(self):
+    if self.tamanho == 0:
+      return False
+    
+    i = 0
+    while i < self.tamanho:
+      self.fila[i] = self.fila[i + 1]
+      i += 1
+    self.tamanho -= 1
+
+  def imprimir(self):
+    print(self.fila[:self.tamanho])
+  
+fila = Fila(5)
+fila.enfileirar(1)
+fila.enfileirar(2)
+fila.imprimir()
+
+fila.desenfileirar()
+fila.imprimir()
